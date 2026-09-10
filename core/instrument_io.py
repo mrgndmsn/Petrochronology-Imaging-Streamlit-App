5d1ea0eb06ba54824fca7049bb57ec651dd91732
Morgan Adamson <mrgndmsn@gmail.com>
2026-09-10 14:04:18 -0700

    Add initial structure for instrument_io.py

--- core/instrument_io.py
+++ core/instrument_io.py
@@ -1,1 +1,31 @@
+"""Probe DAT and Surfer 7 readers."""
+from io import StringIO
+import struct
+import numpy as np
+import pandas as pd
+from .io import unique_columns
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
