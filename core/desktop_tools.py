aca5f4bd2664d29d88b7d5d3b6ecf3e51c443b8a
Morgan Adamson <mrgndmsn@gmail.com>
2026-09-11 11:54:14 -0700

    Move future annotations import to the top

--- core/desktop_tools.py
+++ core/desktop_tools.py
@@ -1,6 +1,7 @@
+from __future__ import annotations
 import numpy as np
 import pandas as pd
-from __future__ import annotations
+
 
 def mineral_fraction_table(
     point_layers, samples=None, mode="pixels", value_column=None
