404b8caadc391d77abf262e7940ba86207d8dd53
Morgan Adamson <mrgndmsn@gmail.com>
2026-09-11 11:47:48 -0700

    Refactor import statements in app.py

--- app.py
+++ app.py
@@ -4,8 +4,7 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 
-from core.io import (matrix_channel_name, matrix_layers, numeric_columns, read_numeric_matrix,
-                     read_table, table_to_layer, table_to_point_layer, validate_assignment)
+from core.io import (matrix_channel_name, matrix_layers, numeric_columns, read_numeric_matrix, read_table, table_to_layer, table_to_point_layer, validate_assignment)
 from core.state import initialize_state
 from core.project_io import load_project, save_project
 from core.definitions import replay_definitions
